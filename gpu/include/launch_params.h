4d72dd5a20a0f4565460f0322bd0a6159ebdae71
Azzouzi <ahmedamineazzouzi.study@gmail.com>
2026-08-22 20:20:11 +0100

    gpu: Define OptiX launch parameters structure

---
+++ gpu/include/launch_params.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <optix.h>
+#include "rt/core/vector3.h"
+#include "rt/cam/perspective_camera.h"
+
+namespace rtx {
+
+    struct LaunchParams {
+        unsigned int width;
+        unsigned int height;
+        rt::Vector3f* outputBuffer;
+    };
+
+    struct NormalsLaunchParams {
+        OptixTraversableHandle iasHandle;
+        rt::PerspectiveCamera camera;
+        unsigned int width;
+        unsigned int height;
+        rt::Vector3f* outputBuffer;
+    };
+
+} // namespace rtx
