2f12bce096bd5f90bb3af1bae56db0d282b6b448
Azzouzi <ahmedamineazzouzi.study@gmail.com>
2026-08-22 20:19:45 +0100

    math: Add device annotations to Orthonormal Basis type

--- include/rt/core/onb.h
+++ include/rt/core/onb.h
@@ -2,24 +2,37 @@
 #include "rt/core/vector3.h"
 #include <cmath>
 
+#if !defined(__CUDACC__) && !defined(__CUDA_ARCH__)
+#ifndef __host__
+#define __host__
+#endif
+#ifndef __device__
+#define __device__
+#endif
+#endif
+
 namespace rt {
 
     struct ONB {
         Vector3f u, v, w;
 
-        explicit ONB(const Vector3f& n) {
+        constexpr __host__ __device__ ONB() = default;
+
+        explicit __host__ __device__ ONB(const Vector3f& n) {
             w = n;
 
             float sign = std::copysign(1.0f, w.z);
 
-            float a {-1.0f / (sign + w.z)}; float b {w.x * w.y * a};
+            float a = -1.0f / (sign + w.z);
+            float b = w.x * w.y * a;
             
             u = Vector3f(1.0f + sign * w.x * w.x * a, sign * b, -sign * w.x);
             v = Vector3f(b, sign + w.y * w.y * a, -w.y);
         }
 
-        Vector3f ToWorld(const Vector3f& local) const {
+        __host__ __device__ Vector3f ToWorld(const Vector3f& local) const {
             return local.x * u + local.y * v + local.z * w;
         }
     };
-}
+
+} // namespace rt
